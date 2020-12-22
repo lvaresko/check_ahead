ab41aaab785d0298c1ae75f9046507545c49187b
unknown <babic.lucija1204@gmail.com>
2020-12-22 19:18:13 +0100

    components + full_logo.png

---
+++ src/components/Auth/Signup.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="signup">
+    <h1>Registration</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm text-left">
+          <form>
+            <div class="form-group">
+              <label form="exampleInputEmail">EmailLUCIJABABIC</label>
+              <input
+                type="email"
+                v-model="email"
+                class="form-control"
+                id="exampleInputEmail"
+                aria-describedby="EmailHelp"
+                placeholder="amy@gmail.com"
+              />
+            </div>
+            <div class="form-group">
+              <label form="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                v-model="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="abc12"
+              />
+              <div class="form-group">
+                <label form="exampleInputPassword2">Repeat password</label>
+                <input
+                  type="password"
+                  v-model="passwordRepeat"
+                  class="form-control"
+                  id="exampleInputpassword2"
+                  placeholder="Retype here your password..."
+                />
+              </div>
+            </div>
+            <button type="button" @click="signup" class="btn btn-primary">
+              Register
+            </button>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { firebase } from "@/firebase";
+
+export default {
+  name: "Signup",
+  data() {
+    return {
+      email: "",
+      //username: '',
+      password: "",
+      passwordRepeat: "",
+    };
+  },
+  methods: {
+    signup() {
+      if ((this.password = this.passwordRepeat)) {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(this.email, this.password)
+          .then(function() {
+            console.log("Uspješna registracija");
+          })
+          .catch(function(error) {
+            console.error("Došlo je do pogreške", error);
+          });
+        console.log("Nastavak");
+      } else {
+        alert("Lozinke se ne podudaraju!");
+      }
+    },
+  },
+};
+</script>
---
+++ src/components/Layout/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item align="left">wohooo</b-nav-item>
+      <b-nav-item align="center">wohooo</b-nav-item>
+      <b-nav-item align="right">woho</b-nav-item>
+    </b-nav>
+
+    <b-navbar variant="faded" type="light">
+      <b-navbar-toggle
+        v-b-toggle.sidebar-backdrop
+        target="nav-collapse"
+        style="border: none;"
+        >blaa</b-navbar-toggle
+      >
+
+      <b-navbar-nav>
+        <img alt="Logo" src="@/assets/logo.png" style="width: 30%;" href="#" />
+      </b-navbar-nav>
+
+      <b-navbar-nav>
+        <b-icon icon="person-fill"></b-icon>
+      </b-navbar-nav>
+    </b-navbar>
+    <b-sidebar
+      id="sidebar-backdrop"
+      title="Sidebar with backdrop"
+      :backdrop-variant="variant"
+      backdrop
+      shadow
+    >
+      <div class="px-3 py-2">
+        <b-form-group label="Backdrop variant" label-for="backdrop-variant">
+          <b-form-select
+            id="backdrop-variant"
+            v-model="variant"
+            :options="variants"
+          ></b-form-select>
+        </b-form-group>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  components: {},
+};
+</script>
+
+<style>
+.b-navbar-nav > {
+  float: none;
+  text-align: center;
+  display: inline-block;
+}
+</style>
